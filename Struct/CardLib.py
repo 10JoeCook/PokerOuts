8a838cf6fb1d94a1eccf78b14299fee0b2e60c2c
Random-Cookie <sefi100@ntlworld.com>
2021-12-18 23:25:25 +0000

    CardLib.py -Refactor     -Renaming of function variables/params     -Removal of print statements -Added documentation from classes and functions -Added TODOs for unimplemented ideas/code blocks

--- Struct/CardLib.py
+++ Struct/CardLib.py
@@ -4,14 +4,23 @@ from functools import cmp_to_key
 
 
 class Card:
+	"""Class to represent a playing card
+	Comparison operators have been implemented for sorting
+	"""
 	def __init__(self, suit: int, rank: int) -> None:
 		self.suit = suit
 		self.rank = rank
 
 	def __str__(self) -> str:
+		"""Return human-readable card name
+		:return: card name in format "<rank> of <suit>"
+		"""
 		return self.get_rank() + " of " + self.get_suit()
 
 	def __int__(self) -> int:
+		""""Return the card number
+		cards are ordered by rank, sub-ordered by suit
+		"""
 		return (self.rank * 4) + self.suit
 
 	def __eq__(self, other) -> bool:
@@ -49,32 +58,50 @@ class Card:
 
 
 class Deck:
+	"""Class to represent a deck of playing cards
+	"""
 	def __init__(self) -> None:
 		self. cards = []
 
 	def generate_standard_deck(self) -> None:
-		for i in range(0, 4):
-			for j in range(0, 13):
-				self.cards.append(Card(i, j))
+		"""Generate a standard deck of playing cards
+		🃑 < 🃁 < 🂱 < 🂡
+		"""
+		for rank in range(0, 13):
+			for suit in range(0, 4):
+				self.cards.append(Card(rank, suit))
 
 	def shuffle(self) -> None:
+		"""Shuffle the Deck
+		"""
 		random.shuffle(self.cards)
 
 	def draw(self) -> Card:
+		"""Draw a card from the deck
+		:return: the drawn card
+		"""
 		return self.cards.pop()
 
-	def deal(self, no_players: int, no_cards: int) -> [[Card]]:
-		# hands
-		hs = []
-		for i in range(no_players):
-			# hand
-			h = []
+	def deal(self, no_hands: int, no_cards: int) -> [[Card]]:
+		"""Deal hands of x size to y players
+		:param no_hands: number of hands to deal
+		:param no_cards: number of cards in each hand
+		:return: list of hands
+		"""
+		hands = []
+		for i in range(no_hands):
+			hand = []
 			for j in range(no_cards):
-				h.append(self.draw())
-			hs.append(h)
-		return hs
+				hand.append(self.draw())
+			hands.append(hand)
+		return hands
 
 	def setup(self, no_players: int, no_cards: int) -> [[Card]]:
+		"""Generate a standard deck, shuffle it then deal
+		:param no_players: number of players to deal hands to
+		:param no_cards: number of cards in each hand
+		:return: dealt cards
+		"""
 		self.generate_standard_deck()
 		self.shuffle()
 		return self.deal(no_players, no_cards)
@@ -83,6 +110,14 @@ class Deck:
 class CardUtils:
 	@staticmethod
 	def rank_no_to_name(rank_no: int) -> str:
+		"""Converts from rank number to a string name
+		This function is the only thing that defines the order of the ranks
+		It alone controls which suit number corresponds to which of the playing card ranks
+		i.e Chinese card games often use 2 as the highest card
+		:param rank_no: the rank number used in Card
+		:return: The name of the corresponding rank
+		:raises: Value error: rank no is not valid
+		"""
 		if rank_no == 0:
 			return "two"
 		elif rank_no == 1:
@@ -113,6 +148,13 @@ class CardUtils:
 
 	@staticmethod
 	def suit_no_to_name(suit_no: int) -> str:
+		"""Converts from suit number to a string name
+		This function is the only thing that defines the order of the suits
+		It alone controls which suit number corresponds to which of the playing card suits
+		:param suit_no: the suit number used in Card
+		:return: The name of the corresponding suit
+		:raises: Value error: suit no is not valid
+		"""
 		if suit_no == 0:
 			return "clubs"
 		if suit_no == 1:
@@ -125,77 +167,108 @@ class CardUtils:
 
 	@staticmethod
 	def find_highest(cards: [Card]) -> Card:
+		"""Return the highest card from a list
+		Currently uses O(n) linear search as collections will be small
+		Also cards could be unsorted, if cards were always sorted O(1) search could be used
+		:param cards: Cards to be searched
+		:return: highest card from the collection
+		"""
 		highest_card = cards[0]
 		for card in cards:
-			if int(card) > int(highest_card):
+			if card > highest_card:
 				highest_card = card
 		return highest_card
 
 	@staticmethod
 	def find_highest_x(cards: [Card], x: int) -> [Card]:
-		r = []
-		crds = cards.copy()
+		"""Find the highest x cards from the list of cards
+		:param cards: list of cards to search
+		:param x: number of cards to find
+		:return: list containing the highest x cards
+		"""
+		highest_x = []
+		cards_cpy = cards.copy()
 		for i in range(x):
-			hc = CardUtils.find_highest(crds)
-			crds.remove(hc)
-			r.append(hc)
-		return r
+			hc = CardUtils.find_highest(cards_cpy)
+			cards_cpy.remove(hc)
+			highest_x.append(hc)
+		return highest_x
 
 
 class HandUtils:
 	@staticmethod
 	def rank_hand(hand: [Card]):
+		"""Rank a 5 card poker hand, lowest rank is best
+		Ranking is as follows:
+		Hand           |Ranking
+		---------------|-------
+		Royal Flush    |0
+		Straight Flush |1
+		Four of a kind |2
+		Full House     |3
+		Flush          |4
+		Straight       |5
+		Three of a kind|6
+		Two Pair       |7
+		Pair           |8
+		High Card      |9
+		:param hand: 5 card hand to rank
+		:return: rank of the hand
+		"""
+		# TODO: implement more total ordering: hand type > group rank/highest card in flush/straight > kicker rank
 		if HandUtils.is_royal_flush(hand):
-			print("Royal Flush")
 			return 0
 		if HandUtils.are_consecutive(hand) and HandUtils.share_suit(hand):
-			print("Straight Flush")
 			return 1
-		gs = HandUtils.find_groups(hand)
-		# quads
-		if len(gs) == 1 and gs[0][0] == 4:
-			print("Quads")
+		groups = HandUtils.find_groups(hand)
+		if len(groups) == 1 and groups[0][0] == 4:
 			return 2
-		# full house
-		if len(gs) == 2 and (gs[0][0] == 3 and gs[1][0] == 2 or gs[0][0] == 2 and gs[1][0] == 3):  # possible optimisation
-			print("Full house")
+		if len(groups) == 2 and (groups[0][0] == 3 and groups[1][0] == 2 or groups[0][0] == 2 and groups[1][0] == 3):  # possible optimisation
 			return 3
 		if HandUtils.share_suit(hand):
-			print("Flush")
 			return 4
 		if HandUtils.are_consecutive(hand):
-			print("Straight")
 			return 5
-		if len(gs) == 1 and gs[0][0] == 3:
-			print("Trips")
+		if len(groups) == 1 and groups[0][0] == 3:
 			return 6
-		if len(gs) == 2 and gs[0][0] == 2 and gs[1][0] == 2:
-			print("Two Pair")
+		if len(groups) == 2 and groups[0][0] == 2 and groups[1][0] == 2:
 			return 7
-		if len(gs) == 1:
-			print("Pair")
+		if len(groups) == 1:
 			return 8
-		print("High Card")
+		# if no hands are found, return 9 representing a high card
 		return 9
 
 	@staticmethod
 	def are_consecutive(cards: [Card]) -> bool:
-		crds = sorted(cards, key=cmp_to_key(lambda item1, item2: item1.get_rank_no() - item2.get_rank_no()))
-		for i in range(len(cards) - 1):
-			if not(crds[i].get_rank_no() + 1 == crds[i + 1].get_rank_no() or (crds[i].get_rank_no() == 3 and crds[i + 1].get_rank_no() == 12)):
+		"""Check if a list cards are consecutive
+		Ace can be used at start or end but cannot wraparound
+		:param cards: cards to check
+		:return: True if cards are consecutive, false otherwise
+		"""
+		sorted_cards = sorted(cards, key=cmp_to_key(lambda card1, card2: card1.get_rank_no() - card2.get_rank_no()))
+		for i in range(len(sorted_cards) - 1):
+			if not(sorted_cards[i].get_rank_no() + 1 == sorted_cards[i + 1].get_rank_no() or (sorted_cards[i].get_rank_no() == 3 and sorted_cards[i + 1].get_rank_no() == 12)):
 				return False
 		return True
 
 	@staticmethod
 	def share_suit(cards: [Card]) -> bool:
-		s = cards[0].get_suit_no()
+		"""Check if a list of cards shares a suit
+		:param cards: cards to check
+		:return: True if cards share suit, false otherwise
+		"""
+		suit = cards[0].get_suit_no()
 		for i in range(len(cards)):
-			if cards[i].get_suit_no() != s:
+			if cards[i].get_suit_no() != suit:
 				return False
 		return True
 
 	@staticmethod
 	def is_royal_flush(cards: [Card]) -> bool:
+		"""Check if cards form a royal flush
+		:param cards: cards too check
+		:return: true if cards from a royal flush, false otherwise
+		"""
 		if HandUtils.are_consecutive(cards) and HandUtils.share_suit(cards):
 			if sorted(cards, key=cmp_to_key(lambda item1, item2: item1.get_rank_no() - item2.get_rank_no()))[0].get_rank_no() == 8:
 				return True
@@ -203,58 +276,74 @@ class HandUtils:
 
 	@staticmethod
 	def find_groups(cards: [Card]) -> [(chr, int)]:
-		# ranks
-		r = []
-		# groups
-		g = []
+		"""Find groups of cards within a list of cards
+		Possible groups are:
+		Four of a kind
+		Three of a kind
+		Pair
+		:param cards: List of cards to search
+		:return: list containing the groups present in the list of cards
+		"""
+		ranks = []
+		groups = []
 		for card in cards:
-			r.append(card.get_rank_no())
-		c = Counter(r)
-		tl = list(dict.fromkeys(c.elements()))
-		# i = rank
-		# c[i] = count
-		for i in tl:
-			if c[i] == 2:
-				g.append((2, i))
-			elif c[i] == 3:
-				g.append((3, i))
-			elif c[i] == 4:
-				g.append((4, i))
-		return g
+			ranks.append(card.get_rank_no())
+		card_counter = Counter(ranks)
+		ranks = list(dict.fromkeys(card_counter.elements()))
+		for rank in ranks:
+			if card_counter[rank] == 2:
+				groups.append((2, rank))
+			elif card_counter[rank] == 3:
+				groups.append((3, rank))
+			elif card_counter[rank] == 4:
+				groups.append((4, rank))
+		return groups
 
 	@staticmethod
 	def make_group_hand(cards: [Card], groups: [(chr, int)]) -> [(chr, int)]:
+		"""Form a five card hand from a set of 7 cards (hand + table cards) that contains groups
+		:param cards: 7 cards (hand + table)
+		:param groups: groups present within the cards
+		:return: 5 card hand with the best groups
+		"""
 		hand = []
-		s_groups = list(reversed(sorted(groups, key=lambda x: x[0])))
-		crds = cards.copy()
+		sorted_groups = list(reversed(sorted(groups, key=lambda x: x[0])))
+		cards_cpy = cards.copy()
 		# quads
-		if s_groups[0][0] == 4:
-			hand += HandUtils.__remove_group(crds, s_groups[0])
-			hand.append(CardUtils.find_highest(crds))
+		if sorted_groups[0][0] == 4:
+			hand += HandUtils.__remove_group(cards_cpy, sorted_groups[0])
+			hand.append(CardUtils.find_highest(cards_cpy))
 		# trips
-		if s_groups[0][0] == 3:
+		if sorted_groups[0][0] == 3:
 			# pick best three of a kind
-			if s_groups[1] == 3 and s_groups[1][1] > s_groups[0][1]:
-				s_groups.remove(s_groups[0])
+			if sorted_groups[1] == 3 and sorted_groups[1][1] > sorted_groups[0][1]:
+				sorted_groups.remove(sorted_groups[0])
 			else:
-				s_groups.remove(s_groups[1])
-			hand += HandUtils.__remove_group(crds, s_groups[0])
-			if len(s_groups) > 2:
+				sorted_groups.remove(sorted_groups[1])
+			hand += HandUtils.__remove_group(cards_cpy, sorted_groups[0])
+			if len(sorted_groups) > 2:
 				pass
-			elif len(s_groups) == 1:
+				# TODO:IMPLEMENT picking teo best pair
+			elif len(sorted_groups) > 1:
 				pass
+				# TODO: IMPLEMENT append pair other cards
 			else:
-				hand += CardUtils.find_highest_x(crds, 2)
+				hand += CardUtils.find_highest_x(cards_cpy, 2)
 		return hand
 
 	@staticmethod
 	def __remove_group(cards: [Card], group: ()) -> [Card]:
-		r = []
+		"""private function to remove cards contained in a group from a list of cards
+		:param cards: cards to remove from
+		:param group: group to remove
+		:return: list of cards with cards in the group removed
+		"""
+		ret = []
 		i = 0
 		while i < len(cards):
 			if cards[i].get_rank_no() == group[1]:
-				r.append(cards[i])
+				ret.append(cards[i])
 				cards.remove(cards[i])
 				i -= 1
 			i += 1
-		return r
+		return ret
